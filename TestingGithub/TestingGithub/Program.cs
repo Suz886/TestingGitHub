a752173157a03a326d95a901013158b66d438178
Suz886 <blixempies.r@gmail.com>
2025-11-07 13:03:59 +0200

    Add project files.

---
+++ TestingGithub/TestingGithub/Program.cs
@@ -0,0 +1,33 @@
+using TestingGithub.Client.Pages;
+using TestingGithub.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TestingGithub.Client._Imports).Assembly);
+
+app.Run();
